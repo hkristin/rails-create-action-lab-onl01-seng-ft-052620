9b9680d9c031e99abf76efa13362dccecca02321
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-03 02:54:47 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,7 @@ class StudentsController < ApplicationController
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
     @student.save
-    redirect to student_path(@student)
+    redirect to new_student_path(@student)
   end
 
 end
